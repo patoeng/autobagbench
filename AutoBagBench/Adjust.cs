db301a97e21f691579bc2bbe2ff3e57ae7b5a363
anom <patoeng@gmail.com>
2017-09-02 11:08:56 +0700

    Add adjust

---
+++ AutoBagBench/Adjust.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AutoBagBench
+{
+    public enum AdjusmentMode
+    {
+        None,
+        Kurangi,
+        Tambah
+    }
+    public partial class Adjust : Form
+    {
+        public Adjust()
+        {
+            InitializeComponent();
+        }
+        public AdjusmentMode ActionToTake { get; protected set; }
+        public int AdjustmentQty { get; protected set; }
+        private void Adjust_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+            if (e.KeyValue >= 48 && e.KeyValue <= 57)
+            {
+                txtAdjust.Text += Convert.ToChar(e.KeyValue);
+                txtAdjust.Text = txtAdjust.Text.TrimStart('0');
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtAdjust.Text = @"0";
+        }
+
+        private void btnKurangi_Click(object sender, EventArgs e)
+        {
+            AdjustmentQty = ConvertToInt(txtAdjust.Text);
+            ActionToTake = AdjusmentMode.Kurangi;
+            Close();
+        }
+
+        private int ConvertToInt(string data)
+        {
+            try
+            {
+                var j = Convert.ToInt32(data);
+                return j;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            AdjustmentQty = ConvertToInt(txtAdjust.Text);
+            ActionToTake = AdjusmentMode.Tambah;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            ActionToTake = AdjusmentMode.None;
+            Close();
+        }
+
+        private void Adjust_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void Adjust_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTmbl0_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button) sender;
+            switch (btn.Name)
+            {
+                case "btnTmbl0": SendKeys.Send("0");
+                    break;
+                case "btnTmbl1":
+                    SendKeys.Send("1");
+                    break;
+                case "btnTmbl2":
+                    SendKeys.Send("2");
+                    break;
+                case "btnTmbl3":
+                    SendKeys.Send("3");
+                    break;
+                case "btnTmbl4":
+                    SendKeys.Send("4");
+                    break;
+                case "btnTmbl5":
+                    SendKeys.Send("5");
+                    break;
+                case "btnTmbl6":
+                    SendKeys.Send("6");
+                    break;
+                case "btnTmbl7":
+                    SendKeys.Send("7");
+                    break;
+                case "btnTmbl8":
+                    SendKeys.Send("8");
+                    break;
+                case "btnTmbl9":
+                    SendKeys.Send("9");
+                    break;
+            }
+        }
+    }
+}
---
+++ AutoBagBench/Adjust.designer.cs
@@ -0,0 +1,266 @@
+﻿namespace AutoBagBench
+{
+    partial class Adjust
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAdjust = new System.Windows.Forms.TextBox();
+            this.btnKurangi = new System.Windows.Forms.Button();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnTmbl1 = new System.Windows.Forms.Button();
+            this.btnTmbl2 = new System.Windows.Forms.Button();
+            this.btnTmbl3 = new System.Windows.Forms.Button();
+            this.btnTmbl4 = new System.Windows.Forms.Button();
+            this.btnTmbl5 = new System.Windows.Forms.Button();
+            this.btnTmbl6 = new System.Windows.Forms.Button();
+            this.btnTmbl7 = new System.Windows.Forms.Button();
+            this.btnTmbl8 = new System.Windows.Forms.Button();
+            this.btnTmbl9 = new System.Windows.Forms.Button();
+            this.btnTmbl0 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAdjust
+            // 
+            this.txtAdjust.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtAdjust.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAdjust.Location = new System.Drawing.Point(19, 12);
+            this.txtAdjust.Name = "txtAdjust";
+            this.txtAdjust.ReadOnly = true;
+            this.txtAdjust.Size = new System.Drawing.Size(172, 47);
+            this.txtAdjust.TabIndex = 0;
+            this.txtAdjust.Text = "0";
+            this.txtAdjust.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnKurangi
+            // 
+            this.btnKurangi.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKurangi.Location = new System.Drawing.Point(7, 337);
+            this.btnKurangi.Name = "btnKurangi";
+            this.btnKurangi.Size = new System.Drawing.Size(85, 43);
+            this.btnKurangi.TabIndex = 1;
+            this.btnKurangi.Text = "KURANGI";
+            this.btnKurangi.UseVisualStyleBackColor = true;
+            this.btnKurangi.Click += new System.EventHandler(this.btnKurangi_Click);
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTambah.Location = new System.Drawing.Point(98, 337);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(92, 43);
+            this.btnTambah.TabIndex = 1;
+            this.btnTambah.Text = "TAMBAHI";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(197, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(57, 47);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(196, 337);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(70, 43);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "CANCEL";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnTmbl1
+            // 
+            this.btnTmbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTmbl1.Location = new System.Drawing.Point(19, 202);
+            this.btnTmbl1.Name = "btnTmbl1";
+            this.btnTmbl1.Size = new System.Drawing.Size(75, 55);
+            this.btnTmbl1.TabIndex = 2;
+            this.btnTmbl1.Text = "1";
+            this.btnTmbl1.UseVisualStyleBackColor = true;
+            this.btnTmbl1.Click += new System.EventHandler(this.btnTmbl0_Click);
+            // 
+            // btnTmbl2
+            // 
+            this.btnTmbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTmbl2.Location = new System.Drawing.Point(98, 202);
+            this.btnTmbl2.Name = "btnTmbl2";
+            this.btnTmbl2.Size = new System.Drawing.Size(75, 55);
+            this.btnTmbl2.TabIndex = 2;
+            this.btnTmbl2.Text = "2";
+            this.btnTmbl2.UseVisualStyleBackColor = true;
+            this.btnTmbl2.Click += new System.EventHandler(this.btnTmbl0_Click);
+            // 
+            // btnTmbl3
+            // 
+            this.btnTmbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTmbl3.Location = new System.Drawing.Point(179, 202);
+            this.btnTmbl3.Name = "btnTmbl3";
+            this.btnTmbl3.Size = new System.Drawing.Size(75, 55);
+            this.btnTmbl3.TabIndex = 2;
+            this.btnTmbl3.Text = "3";
+            this.btnTmbl3.UseVisualStyleBackColor = true;
+            this.btnTmbl3.Click += new System.EventHandler(this.btnTmbl0_Click);
+            // 
+            // btnTmbl4
+            // 
+            this.btnTmbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTmbl4.Location = new System.Drawing.Point(19, 141);
+            this.btnTmbl4.Name = "btnTmbl4";
+            this.btnTmbl4.Size = new System.Drawing.Size(75, 55);
+            this.btnTmbl4.TabIndex = 2;
+            this.btnTmbl4.Text = "4";
+            this.btnTmbl4.UseVisualStyleBackColor = true;
+            this.btnTmbl4.Click += new System.EventHandler(this.btnTmbl0_Click);
+            // 
+            // btnTmbl5
+            // 
+            this.btnTmbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTmbl5.Location = new System.Drawing.Point(98, 141);
+            this.btnTmbl5.Name = "btnTmbl5";
+            this.btnTmbl5.Size = new System.Drawing.Size(75, 55);
+            this.btnTmbl5.TabIndex = 2;
+            this.btnTmbl5.Text = "5";
+            this.btnTmbl5.UseVisualStyleBackColor = true;
+            this.btnTmbl5.Click += new System.EventHandler(this.btnTmbl0_Click);
+            // 
+            // btnTmbl6
+            // 
+            this.btnTmbl6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTmbl6.Location = new System.Drawing.Point(179, 141);
+            this.btnTmbl6.Name = "btnTmbl6";
+            this.btnTmbl6.Size = new System.Drawing.Size(75, 55);
+            this.btnTmbl6.TabIndex = 2;
+            this.btnTmbl6.Text = "6";
+            this.btnTmbl6.UseVisualStyleBackColor = true;
+            this.btnTmbl6.Click += new System.EventHandler(this.btnTmbl0_Click);
+            // 
+            // btnTmbl7
+            // 
+            this.btnTmbl7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTmbl7.Location = new System.Drawing.Point(19, 80);
+            this.btnTmbl7.Name = "btnTmbl7";
+            this.btnTmbl7.Size = new System.Drawing.Size(75, 55);
+            this.btnTmbl7.TabIndex = 2;
+            this.btnTmbl7.Text = "7";
+            this.btnTmbl7.UseVisualStyleBackColor = true;
+            this.btnTmbl7.Click += new System.EventHandler(this.btnTmbl0_Click);
+            // 
+            // btnTmbl8
+            // 
+            this.btnTmbl8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTmbl8.Location = new System.Drawing.Point(98, 80);
+            this.btnTmbl8.Name = "btnTmbl8";
+            this.btnTmbl8.Size = new System.Drawing.Size(75, 55);
+            this.btnTmbl8.TabIndex = 2;
+            this.btnTmbl8.Text = "8";
+            this.btnTmbl8.UseVisualStyleBackColor = true;
+            this.btnTmbl8.Click += new System.EventHandler(this.btnTmbl0_Click);
+            // 
+            // btnTmbl9
+            // 
+            this.btnTmbl9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTmbl9.Location = new System.Drawing.Point(179, 80);
+            this.btnTmbl9.Name = "btnTmbl9";
+            this.btnTmbl9.Size = new System.Drawing.Size(75, 55);
+            this.btnTmbl9.TabIndex = 2;
+            this.btnTmbl9.Text = "9";
+            this.btnTmbl9.UseVisualStyleBackColor = true;
+            this.btnTmbl9.Click += new System.EventHandler(this.btnTmbl0_Click);
+            // 
+            // btnTmbl0
+            // 
+            this.btnTmbl0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTmbl0.Location = new System.Drawing.Point(19, 262);
+            this.btnTmbl0.Name = "btnTmbl0";
+            this.btnTmbl0.Size = new System.Drawing.Size(154, 55);
+            this.btnTmbl0.TabIndex = 2;
+            this.btnTmbl0.Text = "0";
+            this.btnTmbl0.UseVisualStyleBackColor = true;
+            this.btnTmbl0.Click += new System.EventHandler(this.btnTmbl0_Click);
+            // 
+            // Adjust
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 392);
+            this.Controls.Add(this.btnTmbl9);
+            this.Controls.Add(this.btnTmbl6);
+            this.Controls.Add(this.btnTmbl3);
+            this.Controls.Add(this.btnTmbl8);
+            this.Controls.Add(this.btnTmbl5);
+            this.Controls.Add(this.btnTmbl0);
+            this.Controls.Add(this.btnTmbl2);
+            this.Controls.Add(this.btnTmbl7);
+            this.Controls.Add(this.btnTmbl4);
+            this.Controls.Add(this.btnTmbl1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.btnKurangi);
+            this.Controls.Add(this.txtAdjust);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Adjust";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Adjust";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Adjust_FormClosed);
+            this.Load += new System.EventHandler(this.Adjust_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Adjust_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAdjust;
+        private System.Windows.Forms.Button btnKurangi;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnTmbl1;
+        private System.Windows.Forms.Button btnTmbl2;
+        private System.Windows.Forms.Button btnTmbl3;
+        private System.Windows.Forms.Button btnTmbl4;
+        private System.Windows.Forms.Button btnTmbl5;
+        private System.Windows.Forms.Button btnTmbl6;
+        private System.Windows.Forms.Button btnTmbl7;
+        private System.Windows.Forms.Button btnTmbl8;
+        private System.Windows.Forms.Button btnTmbl9;
+        private System.Windows.Forms.Button btnTmbl0;
+    }
+}
